8bac682b23e7ea3f92507b1741e3805ca99978bb
Dimitar <dimitar.mandarliev@gmail.com>
2025-03-16 09:14:58 +0200

    Add project management components and layout updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NoProjectSelected />
     </main>
   );
 }
