1e3f5cbdc3bed3f79f462b690beb6168544369aa
Dimitar <dimitar.mandarliev@gmail.com>
2025-03-17 08:55:31 +0200

    Implement task deletion functionality in task management

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
